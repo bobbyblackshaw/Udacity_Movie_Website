4a4dbddbfc0087e4a08b1ef2c609c964e4e8e9be
bobbyblackshaw <bobbyblackshaw@users.noreply.github.com>
2016-07-15 14:55:21 -0400

    Update class_lesson.py

--- class_lesson.py
+++ class_lesson.py
@@ -9,11 +9,15 @@ import webbrowser
 
 class Movie():
     def __init__(self,movie_title,movie_storyline,poster_image,
-                 trailer_youtube):
+                 trailer_youtube, imdb_rating, year_of_release):
+        '''Initializes all the variables that will be used'''             
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.imdb_rating = imdb_rating
+        self.year_of_release = year_of_release
         
     def show_trailer(self):
+        '''Uses the webbrowser function to open the youtube video trailer'''
         webbrowser.open(self.trailer_youtube_url)
